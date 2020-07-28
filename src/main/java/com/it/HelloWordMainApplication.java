38dc21189c45b1d2ccc8b0d788cf6a0f4d48185f
yyd <ys9803@163.com>
2020-07-28 14:29:03 +0800

    start_springboot

---
+++ src/main/java/com/it/HelloWordMainApplication.java
@@ -0,0 +1,13 @@
+package com.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWordMainApplication {
+
+    public static void main(String[] args) {
+        //spring应用启动起来
+        SpringApplication.run(HelloWordMainApplication.class,args);
+    }
+}
